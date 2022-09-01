6e46e00a62b6e40f25e78595d7caf9fe4fd6c9b0
okoestner <okoestner@web.de>
2022-09-01 20:50:19 +0200

    WorkDir as parameter applied

--- generator.sh
+++ generator.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
+clear
+#set -xe
+set -e
 
-set -xe
+echo $1
+if [[ -z "$1" ]]
+then
+  echo "Missing parameter."
+  echo "Run the tool with the installation directory as first parameter."
+  exit 1
+fi
+
+installation_dir=$1 #"/home/sergio/azure-pricer/"
+lastChar=${installation_dir: -1}
+
+if [[ $lastChar != '/' ]]
+then
+  installation_dir=$installation_dir"/"
+fi
 
 day_of_today=$(date +%Y%m%d)
-installation_dir="/home/sergio/azure-pricer/"
 excel_files_dir=$installation_dir"output/"
 max_number_of_xls_files=30
 
-excel_file_of_today=$excel_files_dir"Azure-Quote-Tool-$day_of_today.xlsx"
+excel_file_of_today=$excel_files_dir"TSI-AzQuote-$day_of_today.xlsx"
 readme_file_template=$excel_files_dir"README.MD.template"
 readme_file=$installation_dir"README.MD"
+
+echo "------------------------------------------------------------------------"
+echo "Excel file of today: $excel_file_of_today"
+echo "Readme-Template: $readme_file_template"
+echo "Readme-File: $readme_file"
+echo "------------------------------------------------------------------------"
 
 cd $installation_dir
 
 git pull
 echo "UPDATING CODE FROM REPO"
+
+exit 0
 
 
 python3 $installation_dir"xls_generator.py" $excel_file_of_today 
